06f55a09dcdb70b2db740ce339a917d98b34d33a
sachinmadapur1997 <161839001+sachinmadapur1997@users.noreply.github.com>
2024-03-05 21:35:13 +0530

    Update demo.java

--- virat/src/virat/demo.java
+++ virat/src/virat/demo.java
@@ -5,7 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 	 System.out.println("hello world");
 	 System.out.println("hi world");
-	 System.out.println("hi hlo");
+	 System.out.println("hi rcb hlo");
 
 	}
 
